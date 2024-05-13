213e911cd0fdda1c0191633648d64402a0c17cd7
vinuji2 <vingg80@gmail.com>
2024-05-13 15:26:17 +0530

    crud completed

---
+++ app/src/main/java/com/example/crud_app/AddNoteActivity.kt
@@ -0,0 +1,34 @@
+package com.example.crud_app
+
+import NotesDatabaseHelper
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.crud_app.databinding.ActivityAddNoteBinding
+
+class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityAddNoteBinding
+    private lateinit var db: NotesDatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener{
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditButton.text.toString()
+            val note = Note(0,title,content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this,"Note Saved",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/crud_app/Note.kt
@@ -0,0 +1,3 @@
+package com.example.crud_app
+
+data class Note(val id:Int,val title:String,val content:String )
